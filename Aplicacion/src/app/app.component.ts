0eb5bca7e1032c8b53dbc3361474fe1de61f5356
Adrian Eguez <eadepto@hotmail.com>
2018-07-02 12:03:41 -0500

    property event binding

--- Aplicacion/src/app/app.component.ts
+++ Aplicacion/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [
+    './app.component.css'
+  ]
 })
 export class AppComponent {
-  title = 'appasdfjhgasdjhkfgajkhsdgfjhasgdjhfgasjkhdfghjkasdgfjhgasdjhfgjkhsadghjkfsdgakhjgasdjkhgfkjhsadgfkjhasdgkjhfgasdkjhfgjkhasdgfkjhasdgfjkhsadgjkfhgsadjkhfgjkahsdgfjkhsadgfjkhasgdjhkfgasdjkhfgjkhsadgfjhkasdgfjkhasgdfjkhgasdjkhfgasjkdhgfjkhasgdjkhfgsadjkhfgsajkhdfgkjhsadgfjkhasdgjhkfgasdjkhfgjkhasdgfkhjkjgh';
+
+  public nombre: string = 'Adrian';
+
+  apellido = 'Eguez';
+
+  mostrar = false;
+
+  arregloNumeros = [5, 4, 3, 2, 1];
+
+  nombreImagen = 'epn-lloron.jpg';
+
+  anchoImagen = 400;
+
+  cambiarTamanoImagen(tamanoCambio: number) {
+    this.anchoImagen += tamanoCambio;
+  }
+
 }
