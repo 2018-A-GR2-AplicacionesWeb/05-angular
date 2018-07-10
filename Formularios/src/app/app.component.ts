f2d27fcb67720315632a2550d56fbac2d5fc314f
Adrian Eguez <eadepto@hotmail.com>
2018-07-10 17:30:21 -0500

    cambios finales de confirmacion de password

--- Formularios/src/app/app.component.ts
+++ Formularios/src/app/app.component.ts
@@ -9,10 +9,25 @@ export class AppComponent {
   title = 'app';
 
   email;
+  password;
+  passwordConfirmation;
 
 
   crearUsuario(formulario) {
     console.log(formulario);
+    const controles = formulario.controls;
+    const password = controles.password.value;
+    const passwordConfirmation = controles.passwordConfirmation.value;
+
+    if (password === passwordConfirmation) {
+      alert('Muy bien!');
+    } else {
+      this.password = undefined;
+      this.passwordConfirmation = undefined;
+      alert('Los passwords no son iguales');
+    }
   }
+
+
 }
 
